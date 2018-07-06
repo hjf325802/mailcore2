9ba1bfdda72a359901b1caeff20d0e364e11ae98
huangjf <huangjf@qtec.cn>
2018-07-06 10:25:28 +0800

    修改libetpan commit id

--- scripts/build-libetpan-ios.sh
+++ scripts/build-libetpan-ios.sh
@@ -7,7 +7,7 @@ popd > /dev/null
 . "$scriptpath/include.sh/build-dep.sh"
 
 url="https://github.com/hjf325802/libetpan.git"
-rev=5164ba2ebd3c7cbc7a9230aad32bdf8e24e207de
+rev=376b105f4c38b14f735a8320a1c96dfc507ddb38
 name="libetpan-ios"
 xcode_target="libetpan ios"
 xcode_project="libetpan.xcodeproj"
